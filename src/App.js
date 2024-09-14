3c48fa97792b9cd6f6d20b9c89d13d498f2a471b
sapientia1007 <91904494+sapientia1007@users.noreply.github.com>
2024-09-14 23:52:08 +0900

    api 수정

--- src/App.js
+++ src/App.js
@@ -45,8 +45,8 @@ function App() {
 
   useEffect(() => {
     // const socket = new SockJS("api/coupong");
-    // const socket = new SockJS("http://3.36.109.146:8080/coupong");
-    const socket = new SockJS("http://localhost:8080/coupong");
+    const socket = new SockJS("https://urecacoupon.store/coupong");
+    // const socket = new SockJS("http://localhost:8080/coupong");
     stompClient.current = Stomp.over(socket);
 
     stompClient.current.connect({}, (frame) => {
